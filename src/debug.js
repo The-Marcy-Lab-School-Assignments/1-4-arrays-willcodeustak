64b634b93527223507c0d263255e1747283126ec
WilliamZeng <itswilliamzeng@gmail.com>
2024-04-11 20:58:01 -0400

    finsihed .2

--- src/debug.js
+++ src/debug.js
@@ -1,11 +1,17 @@
 /* eslint-disable no-param-reassign */
 const clearArr = (arr) => {
-  arr = [];
+  // Remove all elements from index 0 to the end of the array
+  while (arr.length > 0) {
+    arr.pop();
+  }
+  return arr;
 };
 
+console.log(clearArr(4, 5, 6, 7));
 const getFirstItem = (array) => {
-  return array.shift();
+  return array[0];
 };
+console.log(getFirstItem([8, 3, 4, 5, 6]));
 
 module.exports = {
   clearArr,
